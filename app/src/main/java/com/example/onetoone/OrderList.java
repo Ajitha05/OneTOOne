3257249cc469a1c86f90c37e328ac292c676cbab
Ajitha05 <2k19cse005@kiot.ac.in>
2023-04-12 18:20:21 +0530

    code

--- app/src/main/java/com/example/onetoone/OrderList.java
+++ app/src/main/java/com/example/onetoone/OrderList.java
@@ -2,13 +2,32 @@ package com.example.onetoone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class OrderList extends AppCompatActivity {
+    ListView lst;
+    TextView detail;
+    private ArrayAdapter<String> adapter;
 
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_list);
+
+        lst = findViewById(R.id.list1);
+        detail = findViewById(R.id.textView);
+
+
+        OrderDao orderDao = ((MyApplication) getApplication()).getDaoSession().getOrderDao();
+
     }
 }
